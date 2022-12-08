4c26c30d165a0e2949216cc8fbc4e40d15764895
Luke Lawlor <lklawlor1@gmail.com>
2022-12-08 16:48:57 -0500

    refactored and cleaned up day 7 part 1

--- 7.c
+++ 7.c
@@ -2,6 +2,12 @@
  * Advent of Code 2022 Day 7
  *
  * Solution by Luke L
+ *
+ * Known limitations:
+ * 	Chained together hidden directories (i.e. ../../../) won't be recognized
+ *	./ won't be recognized
+ *
+ * The program still fulfills the challenge's requirements though.
  */
 
 #include <stdio.h>
@@ -10,12 +16,14 @@
 #include <ctype.h>
 #include <string.h>
 
+// Error message printing
 #define	ERR_MSG_START	"error: "
 #define	ERR_MSG_END	"\n"
 #define	PERR(...)	fprintf(stderr, ERR_MSG_START); \
 			fprintf(stderr, __VA_ARGS__); \
 			fprintf(stderr, ERR_MSG_END)
 
+// Comment to disable info printing
 #define	PRINT_INFO
 
 #ifdef	PRINT_INFO
@@ -28,17 +36,18 @@
 	#define	PINF(...)	{(void)0;}
 #endif
 
+// Prints to stdout with a specified number of whitespace characters before the message
 #define	PTAB(tabs, ...)		for (int i = 0; i < tabs; i++) \
 					putchar(' '); \
 				printf(__VA_ARGS__); \
 				putchar('\n') \
 
-// size of buffer used to read in strings
+// Size of buffer used to read in strings
 #define	BUF_SIZ		200
 
 #define	MAX_SMALL_SIZ	100000LL
 
-typedef struct File{
+typedef struct{
 	char *name;
 	long long size;
 } File;
@@ -47,26 +56,62 @@ typedef struct Directory{
 	char *name;
 	struct Directory *parent;
 	struct Directory **dirs;
-	struct File **files;
+	File **files;
 	long long size;
 } Directory;
 
+// Buffer for reading characters from stdin
+typedef struct{
+	char *dat;
+	size_t len;
+} Buf;
+
+typedef struct{
+	char *start;
+	size_t len;
+} StringView;
+
+// Creates a new directory, calls abort() on error
+Directory *dir_new(const char *name_start, size_t name_len);
+
+// Directory populating functions
 void dir_add_dir(Directory *parent, Directory *child);
 void dir_add_file(Directory *parent, File *to_add);
+
+// Directory searching functions
 Directory *dir_find_dir(Directory *parent, const char *dir_name);
 File *dir_find_file(Directory *parent, const char *file_name);
+
+// Returns the size of a directory
 long long dir_get_size(Directory *parent);
+
+// Prints the files and subdirectories of a directory
 void dir_list(Directory *parent, int tabs);
 
-// calculates the size of directories with a size <= 100000, must be called after all directory sizes have been calculated
+// Calculates the size of directories with a size <= 100000, must be called after all directory sizes have been calculated
 long long dir_calc_small_size(Directory *parent);
+
+// Reads all characters from stdin into a buffer, starting from the len value in the buffer, and stopping once the stop char is found, calls abort() on errors
+void buf_read(Buf *buf, char stop);
+
+// Moves past characters in standard input, calls abort() if characters that are different than the ones supplied are found
+void move_past(const char *str);
+
+// Command handling algorithms
+void handle_cd(Buf *buf, Directory **working_dir, Directory *root);
+void handle_ls(Buf *buf, Directory **working_dir);
 
 int main(void)
 {
+	// Current character being read
 	int c;
 
-	char buf[BUF_SIZ];
-	size_t buf_len = 0;
+	// Buffer used to read in strings
+	char buf_data[BUF_SIZ];
+	Buf buf = {
+		.dat = buf_data,
+		.len = 0,
+	};
 
 	Directory root = {
 		.name = "/",
@@ -78,263 +123,35 @@ int main(void)
 
 	Directory *working_dir = &root;
 
-	// process command line
+	// Process commands
 	while (true)
 	{
-		// detect command prompt
-		if ((c = getchar()) != '$')
-			abort();
-		if ((c = getchar()) != ' ')
-			abort();
-		PINF("found command prompt");
+		// Detect command prompt
+		switch (c = getchar())
+		{
+		case EOF:
+			PINF("found end of file");
+			goto l_reading_finished;
+		case '$':
+			move_past(" ");
+			PINF("found command prompt");
+			break;
+		}
 
-		// get command
-	l_get_command:
+		// Get command
 		switch (c = getchar())
 		{
 		case 'c':
-			{
-				PINF("cd");
-				// cd
-
-				// move past "d "
-				scanf("d ");
-
-				// store dir name in buf
-				buf_len = 0;
-				while ((c = getchar()) != '\n')
-				{
-					switch (c)
-					{
-					case ' ':
-						// there shouldn't be a space in the dir name
-						abort();
-					default:
-						// collect chars in buffer
-						buf[buf_len++] = c;
-						if (buf_len + 1 >= BUF_SIZ)
-						{
-							PERR("dir name exceeded buf size");
-							abort();
-						}
-					}
-				}
-				buf[buf_len++] = '\0';
-				PINF("dir name = %s; wdir = %s; 1up = %s;", buf, working_dir->name);
-
-				// check for special dir names
-				if (buf[0] == '/')
-				{
-					PINF("entering root");
-					working_dir = &root;
-					break;
-				}
-				else if (strncmp(buf, "..", 3) == 0)
-				{
-					PINF("going back 1 dir");
-					if (working_dir->parent != NULL)
-					{
-						working_dir = working_dir->parent;
-						PINF("went back");
-					}
-					else
-					{
-						PINF("didn't went back");
-					}
-					break;
-				}
-
-				// check if the dir exists
-				Directory *d;
-				if ((d = dir_find_dir(working_dir, buf)) == NULL)
-				{
-					// it doesn't, create it
-					if ((d = malloc(sizeof(Directory))) == NULL)
-					{
-						PERR("bad malloc when creating dir");
-						abort();
-					}
-					strncpy(d->name, buf, buf_len);
-					d->dirs = NULL;
-					d->files = NULL;
-					d->size = 0;
-					dir_add_dir(working_dir, d);
-					PINF("new dir added");
-				}
-
-				// add d to working dir, then enter it
-				working_dir = d;
-				PINF(">>> %s", d->name);
-				break;
-			}
+			move_past("d ");
+			handle_cd(&buf, &working_dir, &root);
+			break;
 		case 'l':
-			{
-				// ls
-				PINF("ls");
-
-				// move past "s\n"
-				scanf("s\n");
-
-				while (true)
-				{
-					c = getchar();
-					if (isdigit(c))
-					{
-						// file found
-						PINF("found file");
-
-						// store file size in buf
-						buf_len = 0;
-						buf[buf_len++] = c;
-						while ((c = getchar()) != ' ')
-						{
-							// collect chars in buffer
-							buf[buf_len++] = c;
-							if (buf_len + 1 >= BUF_SIZ)
-							{
-								PERR("file size exceeded buf size");
-								abort();
-							}
-						}
-						buf[buf_len++] = '\0';
-
-						// pointer to string containing file size
-						char *str_size = buf;
-						size_t str_size_len = buf_len;
-
-						// pointer to string containing file name
-						char *str_name = buf + buf_len;
-						size_t str_name_len;
-
-						// store file name after file size in buf
-						while ((c = getchar()) != '\n')
-						{
-							// collect chars in buffer
-							buf[buf_len++] = c;
-							if (buf_len + 1 >= BUF_SIZ)
-							{
-								PERR("file name exceeded buf size");
-								abort();
-							}
-						}
-						buf[buf_len++] = '\0';
-						str_name_len = (buf + buf_len) - str_name;
-
-						PINF("file name = %s", str_name);
-						PINF("file size = %s", str_size);
-						
-						// check if the file exists
-						File *f;
-						if ((f = dir_find_file(working_dir, str_name)) == NULL)
-						{
-							// it doesn't, create it
-							if ((f = malloc(sizeof(File))) == NULL)
-							{
-								PERR("bad malloc when creating file");
-								abort();
-							}
-							if ((f->name = calloc(str_name_len, sizeof(char))) == NULL)
-							{
-								PERR("bad calloc when creating file name");
-								abort();
-							}
-							strncpy(f->name, str_name, (buf + buf_len) - str_name);
-							if ((f->size = atoll(str_size)) == 0)
-							{
-								PERR("size of file was 0");
-								abort();
-							}
-
-							// add it to the working directory
-							dir_add_file(working_dir, f);
-						}
-					}
-					else if (c == 'd')
-					{
-						// subdirectory found
-						PINF("found dir");
-
-						// move past "ir "
-						scanf("ir ");
-						
-						// store dir name in buf
-						buf_len = 0;
-						while ((c = getchar()) != '\n')
-						{
-							switch (c)
-							{
-							case ' ':
-								// there shouldn't be a space in the dir name
-								abort();
-							default:
-								// collect chars in buffer
-								buf[buf_len++] = c;
-								if (buf_len + 1 >= BUF_SIZ)
-								{
-									PERR("file name exceeded buf size");
-									abort();
-								}
-							}
-						}
-						buf[buf_len++] = '\0';
-
-						PINF("dir name = %s", buf);
-
-						// check if the dir exists
-						Directory *d;
-						if ((d = dir_find_dir(working_dir, buf)) == NULL)
-						{
-							PINF("creating dir");
-							// it doesn't, create it
-							if ((d = malloc(sizeof(Directory))) == NULL)
-							{
-								PERR("bad malloc when creating dir");
-								abort();
-							}
-							if ((d->name = calloc(buf_len, sizeof(char))) == NULL)
-							{
-								PERR("bad calloc when creating dir name");
-								abort();
-							}
-							strncpy(d->name, buf, buf_len);
-							d->dirs = NULL;
-							d->files = NULL;
-							d->size = 0;
-							PINF("dir created");
-
-							// add d to the current working directory
-							PINF("added dir");
-							dir_add_dir(working_dir, d);
-						}
-						else
-							PINF("dir already existed");
-					}
-					else if (c == '$')
-					{
-						PINF("get command");
-
-						// new command
-						if ((c = getchar()) != ' ')
-							abort();
-						goto l_get_command;
-					}
-					else if (c == EOF)
-					{
-						PINF("end of file found");
-						goto l_calc_size;
-					}
-					else
-					{
-						PERR("unknown character found");
-						abort();
-					}
-				}
-				break;
-			}
+			move_past("s\n");
+			handle_ls(&buf, &working_dir);
+			break;
 		}
 	}
-l_calc_size:
-	PINF("finished reading command line log :)");
+l_reading_finished:
 	PINF("listing root...");
 	dir_list(&root, 0);
 	PINF("calculating dir sizes...");
@@ -344,11 +161,42 @@ l_calc_size:
 	return 0;
 }
 
+// Creates a new directory, calls abort() on error
+Directory *dir_new(const char *name_start, size_t name_len)
+{
+	Directory *d;
+	if ((d = malloc(sizeof(Directory))) == NULL)
+	{
+		PERR("bad malloc when creating directory");
+		abort();
+	}
+
+	// Initializing name
+	if ((d->name = calloc(name_len, sizeof(char))) == NULL)
+	{
+		PERR("bad calloc when creating directory name");
+		abort();
+	}
+	strncpy(d->name, name_start, name_len);
+
+	// Default values
+	d->dirs = NULL;
+	d->files = NULL;
+
+	// Note: size is calculated later with a separate function dir_get_size()
+	d->size = 0;
+
+	PINF("dir %s created", d->name);
+	return d;
+}
+
+// Directory populating functions
 void dir_add_dir(Directory *parent, Directory *child)
 {
+	PINF("adding dir %s", child->name);
 	if (parent->dirs == NULL)
 	{
-		// parent has no dirs, create array
+		// Parent has no dirs, create array
 		if ((parent->dirs = calloc(2, sizeof(Directory *))) == NULL)
 		{
 			PERR("bad calloc when creating subdirs");
@@ -360,30 +208,31 @@ void dir_add_dir(Directory *parent, Directory *child)
 		return;
 	}
 
-	// find the last space in the dir array
+	// Find the last space in the dir array
 	Directory *d;
 	int i = 0;
 	while ((d = parent->dirs[i]) != NULL)
 		i++;
 	
-	// resize dir array
+	// Resize dir array
 	if ((parent->dirs = reallocarray(parent->dirs, i + 2, sizeof(Directory *))) == NULL)
 	{
 		PERR("bad reallocarray when adding subdir");
 		abort();
 	}
 
-	// link dirs
+	// Link dirs
 	child->parent = parent;
 	parent->dirs[i] = child;
 	parent->dirs[i + 1] = NULL;
-	PINF("dir added");
 }
 
 void dir_add_file(Directory *parent, File *to_add)
 {
+	PINF("adding file %s", to_add->name);
 	if (parent->files == NULL)
 	{
+		// Parent has no files, create array
 		if ((parent->files = calloc(2, sizeof(File *))) == NULL)
 		{
 			PERR("bad calloc when creating files");
@@ -393,20 +242,26 @@ void dir_add_file(Directory *parent, File *to_add)
 		parent->files[1] = NULL;
 		return;
 	}
+	
+	// Find the last space in file array
 	File *f;
 	int i = 0;
 	while ((f = parent->files[i]) != NULL)
 		i++;
+
+	// Resize file array
 	if ((parent->files = reallocarray(parent->files, i + 2, sizeof(File *))) == NULL)
 	{
 		PERR("bad reallocarray when adding file");
 		abort();
 	}
+
+	// Add file
 	parent->files[i] = to_add;
 	parent->files[i + 1] = NULL;
-	PINF("file added");
 }
 
+// Directory searching functions
 Directory *dir_find_dir(Directory *parent, const char *dir_name)
 {
 	if (parent->dirs == NULL)
@@ -433,37 +288,44 @@ File *dir_find_file(Directory *parent, const char *file_name)
 	for (int i = 0; (f = parent->files[i]) != NULL; i++)
 	{
 		if (strncmp(f->name, file_name, name_len) == 0)
+		{
+			PINF("file %s found", f->name);
 			return f;
+		}
 	}
 	return NULL;
 }
 
 long long dir_get_size(Directory *parent)
 {
-	PINF("calculating dir %s", parent->name);
-
+	PINF("calculating size of dir %s", parent->name);
 	long long size = 0;
 
-	// sum all file sizes
+	// Sum all file sizes
 	if (parent->files != NULL)
 	{
 		File *f;
 		for (int i = 0; (f = parent->files[i]) != NULL; i++)
+		{
+			PINF("adding file %s with size %lld", f->name, f->size);
 			size += f->size;
+		}
 	}
-	PINF("files summed");
 
 	// sum all dir sizes (recursion time!)
 	if (parent->dirs != NULL)
 	{
 		Directory *d;
 		for (int i = 0; (d = parent->dirs[i]) != NULL; i++)
-			size += dir_get_size(d);
+		{
+			dir_get_size(d);
+			PINF("adding dir %s with size %lld", d->name, d->size);
+			size += d->size;
+		}
 	}
-	PINF("dirs summed");
 
 	parent->size = size;
-	PINF("size of dir %s = %lld", parent->name, size);
+	PINF("size of dir %s is %lld", parent->name, size);
 	return size;
 }
 
@@ -485,8 +347,8 @@ long long dir_calc_small_size(Directory *parent)
 
 void dir_list(Directory *parent, int tabs)
 {
-	PTAB(tabs, parent->name);
-	tabs += 2;
+	PTAB(tabs, "dir: %s", parent->name);
+	tabs += 4;
 	if (parent->dirs != NULL)
 	{
 		Directory *d;
@@ -501,4 +363,167 @@ void dir_list(Directory *parent, int tabs)
 			PTAB(tabs, "%s", f->name);
 		}
 	}
+}
+
+// Reads all characters from stdin into a buffer, starting from the len value in the buffer, and stopping once the stop char is found, calls abort() on errors
+void buf_read(Buf *buf, char stop)
+{
+	int c;
+	while ((c = getchar()) != stop)
+	{
+		buf->dat[buf->len++] = c;
+
+		// Check if there's enough room to fit the next char or null terminator
+		if (buf->len + 1 >= BUF_SIZ)
+		{
+			PERR("buffer overflowed, try increasing BUF_SIZ");
+			abort();
+		}
+	}
+	buf->dat[buf->len++] = '\0';
+}
+
+// Moves past characters in standard input, calls abort() if characters that are different than the ones supplied are found
+void move_past(const char *str)
+{
+	if (scanf(str) == EOF)
+	{
+		PERR("expected \"%s\" but didn't find it", str);
+		abort();
+	}
+}
+
+// Command handling algorithms
+void handle_cd(Buf *buf, Directory **working_dir, Directory *root)
+{
+	PINF("cd");
+
+	// Store dir name in buf
+	buf->len = 0;
+	buf_read(buf, '\n');
+
+	// Handle special dir names
+	if (strncmp(buf->dat, "/", 2) == 0)
+	{
+		PINF("entering root");
+		*working_dir = root;
+		return;
+	}
+	else if (strncmp(buf->dat, "..", 3) == 0)
+	{
+		PINF("entering parent dir");
+		if ((*working_dir)->parent != NULL)
+			(*working_dir) = (*working_dir)->parent;
+		else
+			PINF("can't go above root");
+		return;
+	}
+
+	// Check if the dir exists
+	Directory *d;
+	if ((d = dir_find_dir(*working_dir, buf->dat)) == NULL)
+	{
+		// It doesn't, create it
+		Directory *d = dir_new(buf->dat, buf->len);
+		dir_add_dir(*working_dir, d);
+	}
+
+	// Enter the dir
+	PINF("entering dir %s", d->name);
+	*working_dir = d;
+}
+
+void handle_ls(Buf *buf, Directory **working_dir)
+{
+	PINF("ls");
+
+	// Continually read contents of dir until next command is reached
+	int c;
+	while ((c = getchar()) != '$')
+	{
+		if (isdigit(c))
+		{
+			PINF("file found");
+
+			// Store file size in buf
+			buf->len = 0;
+			buf->dat[buf->len++] = c;
+			buf_read(buf, ' ');
+
+			// String containing file size
+			StringView str_size = {
+				.start = buf->dat,
+				.len = buf->len,
+			};
+
+			// Pointer to string containing file name
+			StringView str_name = {
+				.start = buf->dat + buf->len,
+				// length is uninitialized for now, as it isn't known yet
+			};
+
+			// store file name after file size in buf
+			buf_read(buf, '\n');
+
+			// Set file name string length
+			str_name.len = (buf->dat + buf->len) - str_name.start;
+
+			// Check if the file exists
+			File *f;
+			if ((f = dir_find_file(*working_dir, str_name.start)) == NULL)
+			{
+				// It doesn't, create it
+				if ((f = malloc(sizeof(File))) == NULL)
+				{
+					PERR("bad malloc when creating file");
+					abort();
+				}
+				if ((f->name = calloc(str_name.len, sizeof(char))) == NULL)
+				{
+					PERR("bad calloc when creating file name");
+					abort();
+				}
+				strncpy(f->name, str_name.start, str_name.len);
+				if ((f->size = atoll(str_size.start)) == 0)
+				{
+					PERR("size of file was 0");
+					abort();
+				}
+
+				// Add it to the working directory
+				dir_add_file(*working_dir, f);
+			}
+		}
+		else if (c == 'd')
+		{
+			PINF("dir found");
+			move_past("ir ");
+			
+			// Store dir name in buf
+			buf->len = 0;
+			buf_read(buf, '\n');
+
+			// Check if the dir exists
+			Directory *d;
+			if ((d = dir_find_dir(*working_dir, buf->dat)) == NULL)
+			{
+				// It doesn't, create it
+				Directory *d = dir_new(buf->dat, buf->len);
+				dir_add_dir(*working_dir, d);
+			}
+		}
+		else if (c == EOF)
+		{
+			ungetc(c, stdin);
+			return;
+		}
+		else
+		{
+			PERR("unknown character found");
+			abort();
+		}
+	}
+
+	// A new command was found, unget the $ and return to the parent routine
+	ungetc(c, stdin);
 }
